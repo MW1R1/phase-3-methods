d77b973ac3cd58e1fa6275556026ace173321b20
MW1R1 <nickmwirigi58@gmail.com>
2023-05-17 23:34:53 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,18 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet (name)
+  puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+  return num1 + num2
+end
+
+def halve(num)
+  if not isinstance(num, int)
+    return Nil
+  return num / 2
+end
+end
